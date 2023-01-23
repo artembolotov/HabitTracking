495a25122b91137055f2e851168106f6c763e56e
Artem Bolotov <teeemon@gmail.com>
2023-01-23 08:39:04 +0300

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by artembolotov on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
